902abd3af338094e2fc40d45cc4f50734b41fb7f
Joe Tam <joebt92@gmail.com>
2018-03-01 18:18:07 -0800

    copied css from openTable to menu

--- client/src/components/MenuItem.jsx
+++ client/src/components/MenuItem.jsx
@@ -4,7 +4,10 @@ import ReactDOM from 'react-dom';
 const MenuItem = props => (
   <div className="menu-item">
     <div className='menu-item-title'>
-      {props.menuItem.menuItemName} ${props.menuItem.menuItemPrice}
+      {props.menuItem.menuItemName.toUpperCase()} 
+      <div className='menu-item-price'>
+        ${props.menuItem.menuItemPrice}     
+      </div>
     </div>
       <p className='menu-item-desc'>{props.menuItem.menuItemDescription}</p>
   </div>
