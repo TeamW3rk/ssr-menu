bed37ebe136e9f5c7c993ba5b31dc6ae46831140
Joe Tam <joebt92@gmail.com>
2018-03-06 15:17:36 -0800

    added rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,10 @@ module.exports = {
   extends: ["plugin:jsx-a11y/recommended", "airbnb"],
   plugins: ["jsx-a11y"],
   rules: {
-    "no-console": "off"
+    "no-console": "off",
+    "no-undef": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/prop-types": 0,
   },
-  env: {"browser": true}
+  env: { browser: true }
 };
