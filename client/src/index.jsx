a92ace4612af9b4cff89c6c8245538ffcc79e6c6
Joe Tam <joebt92@gmail.com>
2018-03-06 15:18:01 -0800

    fixed code with eslint

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -112,4 +112,6 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// ReactDOM.render(<App />, document.getElementById('app'));
+
+export default App;
