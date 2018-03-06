afb9595153b84cedf0187276ab154eee451a90ef
Joe Tam <joebt92@gmail.com>
2018-03-06 15:33:11 -0800

    small changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     "no-undef": "off",
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
     "react/prop-types": 0,
+    "no-unused-vars": "off",
   },
   env: { browser: true }
 };
