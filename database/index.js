2645ff702cb5987fdd0cfdeac45559575fd50c80
Joe Tam <joebt92@gmail.com>
2018-03-06 21:05:32 -0800

    fixed to cater to multiple users

--- database/index.js
+++ database/index.js
@@ -2,10 +2,12 @@ const Sequelize = require('sequelize');
 const helper = require('./helper');
 const faker = require('faker');
 
-const sequelize = new Sequelize('menus', 'Joe', '', {
+const user = 'Joe';
+
+const sequelize = new Sequelize('menus', user, '', {
   host: 'localhost',
-  // comment out port if not using Postgres.app
-  port: 5554,
+  // uncomment port if using Postgres.app
+  // port: 5554,
   dialect: 'postgres',
 });
 
