1db657c4f7c2ce8d0c0d6f39f5c861dc00d2d4bb
ACODE91 <aaron.z.lao@gmail.com>
2018-03-22 19:47:20 -0700

    refactored to have all unique restaurant ids

--- database/cassandraSeed.js
+++ database/cassandraSeed.js
@@ -2,6 +2,8 @@ const cassandra = require('cassandra-driver');
 const async = require('async');
 const helper = require('./helper');
 const faker = require('faker');
+const numCPUs = require('os').cpus().length;
+const cluster = require('cluster');
 
 const client = new cassandra.Client({ contactPoints: ['127.0.0.1'] });
 
@@ -17,58 +19,71 @@ const menuCategoryNames = ['Appetizers', 'Mains', 'Sides', 'Beverages'];
 //   })
 //   .then(() => {
 //     const query = 'CREATE TABLE IF NOT EXISTS test.RestaurantMenuItems ' +
-//       '(id UUID, restaurantId text, menuName text, menuCategoryNames text, menuItemName text, menuItemDescription text, menuItemPrice decimal, PRIMARY KEY (id))';
+//       '(restaurantName varchar, menuName text, menuCategoryNames text, menuItemName text, menuItemDescription text, menuItemPrice decimal, restaurantId int, PRIMARY KEY (restaurantId, menuname, menucategorynames, menuitemdescription))';
 //     return client.execute(query);
 //   }).then((result) => client.shutdown());
 
-var time = new Date().getTime();
+let time = new Date().getTime();
 
-const query = 'INSERT INTO menus.restaurantmenuitems (id, restaurantId, menuName, menuCategoryNames, menuItemName, menuItemDescription, menuItemPrice) VALUES (uuid(), ?, ?, ?, ?, ?, ?)';
+const query = 'INSERT INTO test.restaurantmenuitems (restaurantName, menuName, menuCategoryNames, menuItemName, menuItemDescription, menuItemPrice, restaurantId) VALUES (?, ?, ?, ?, ?, ?, ?)';
 
-var queries = [];
+let queries = [];
 
-function createRestaurantData() {
-  for (let j = 0; j < menuNames.length; j += 1) {
-    for (let k = 0; k < menuCategoryNames.length; k += 1) {
-      for (let l = 1; l <= 9; l += 1) {
+function createRestaurantData(idCount, restaurantName) {
+  for (let j = 0; j < menuNames.length; j++) {
+    for (let k = 0; k < menuCategoryNames.length; k++) {
+      for (let l = 0; l < 8; l++) {
         const queryObject = { query };
-        var params = [];
-  
-        params.push(helper.capFirstLet(faker.lorem.words()));
+        const params = [];
+        params.push(restaurantName);
         params.push(menuNames[j]);
         params.push(menuCategoryNames[k]);
         params.push(helper.capFirstLet(faker.lorem.words()));
         params.push(helper.capFirstLet(faker.lorem.words()));
         params.push(helper.randomPrice());
-    //might be overwriting here.
-        queryObject['params'] = params;
+        params.push(idCount);
+        queryObject.params = params;
         queries.push(queryObject);
       }
     }
   }
-  // createRestaurantData(restaurantId + 1, baseCase);
 }
 
-async function seedDb(count) {
-  if (count > 92592) {
-    console.log('done in ', (new Date().getTime() - time) / 1000, 's :3 ^_^ <3 <(^_^<)');
-    return ;
+ function seedDb(count, pageCounter) {
+  const id = cluster.worker.id;
+  const startPoint = (id - 1) * (100000 / numCPUs) + 1;
+
+  if (count > ((100000 / numCPUs) * id)) {
+    console.log('done in ', (new Date().getTime() - time) / 1000, 's O___O');
+    return;
   }
-  createRestaurantData();
- await client.batch(queries, { prepare: true })
+
+  createRestaurantData((startPoint + pageCounter), helper.capFirstLet(faker.lorem.words()));
+  
+   client.batch(queries, { prepare: true })
     .then(() => {
-    // All queries have been executed successfully
-      
-    seedDb(count + 1);
-    queries = [];
+      queries = [];
+      seedDb(count + 1, pageCounter + 1);
     })
     .catch((err) => {
       console.log(err);
-    // None of the changes have been applied
     });
-};
+}
 
-seedDb(1);
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
- 
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`finished in ${new Date()}`);
+    console.log(`worker ${worker.process.pid} finished`);
+  });
+} else {
+  const id = cluster.worker.id;
+  seedDb((id - 1) * (100000 / numCPUs) + 1, 0);
+  console.log(`Worker ${process.pid} started`);
+}
 
